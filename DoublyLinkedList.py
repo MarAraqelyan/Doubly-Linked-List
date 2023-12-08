d165d4639eb704fd22943728bd632f9184896719
Mariam <mariam.araqelyan2004@gmail.com>
2023-12-09 00:26:00 +0400

    Linked list in modules

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -1,43 +1,39 @@
-class Node:
-    def __init__(self,data):
-        self.data = data
-        self.prev = None
-        self.next = None
+from node import Node
 
 class LinkedList:
     def __init__(self):
-      self.head = None
-      self.tail = None
+        self.head = None
+        self.tail = None
 
     def isempty(self):
-      return self.head is None
+        return self.head is None
 
     def prepend(self,data):
-      node = Node(data)
+        node = Node(data)
 
-      if self.isempty():
-        self.head = self.tail = node
+        if self.isempty():
+            self.head = self.tail = node
 
-      else:
-        self.head.prev = node
-        node.next = self.head
-        self.head = node
+        else:
+            self.head.prev = node
+            node.next = self.head
+            self.head = node
 
     def append(self,data):
         node = Node(data)
 
         if self.isempty():
-          self.head = self.tail = node
+            self.head = self.tail = node
 
         else:
-          current = self.head
+            current = self.head
 
-          while current.next:
-            current = current.next
+            while current.next:
+                current = current.next
 
-          node.prev = current
-          current.next = node
-          self.tail = node
+            node.prev = current
+            current.next = node
+            self.tail = node
 
     def insert_after(self,target_data,data):
         node = Node(data)
@@ -47,60 +43,60 @@ class LinkedList:
             current = current.next
 
         if current and current.next:
-          node.prev = current
-          node.next = current.next
-          current.next = node
+            node.prev = current
+            node.next = current.next
+            current.next = node
 
         elif current:
-          node.prev = current
-          current.next = node
+            node.prev = current
+            current.next = node
 
         else:
-          raise ValueError("The target_data is not in list")
+            raise ValueError("The target_data is not in list")
 
     def insert_before(self,target_data,data):
         node = Node(data)
         current = self.tail
 
         while current and current.data != target_data:
-          current = current.prev
+            current = current.prev
 
         if current and current.prev:
-          node.next = current
-          node.prev = current.prev
-          current.prev.next = node
+            node.next = current
+            node.prev = current.prev
+            current.prev.next = node
 
         elif current:
-          node.next = current
-          current.prev = node
-          self.head = node
+            node.next = current
+            current.prev = node
+            self.head = node
 
         else:
-          raise ValueError("The target_data is not in list")
+            raise ValueError("The target_data is not in list")
 
     def delete(self,data):
-      if self.isempty():
-        raise ValueError("The list is empty")
+        if self.isempty():
+            raise ValueError("The list is empty")
 
-      current = self.head
+        current = self.head
 
-      if self.head.data == data:
-        self.head = self.head.next
-        self.head.next.prev = None
-        return
+        if self.head.data == data:
+            self.head = self.head.next
+            self.head.next.prev = None
+            return
 
-      while current.next and current.next.data != data:
-        current = current.next
+        while current.next and current.next.data != data:
+            current = current.next
 
-      if current.next:
-        current.next = current.next.next
+        if current.next:
+            current.next = current.next.next
 
-      else:
-        raise ValueError("The data is not in list")
+        else:
+            raise ValueError("The data is not in list")
 
     def display(self):
-      current = self.head
-      while current:
-        print(current.data,end = "<-->")
-        current = current.next
-      print()
+        current = self.head
+        while current:
+            print(current.data,end = "<-->")
+            current = current.next
+        print()
---
+++ main.py
@@ -0,0 +1,16 @@
+from doublyLinkedList import LinkedList
+
+def main():
+
+    linked_list = LinkedList()
+
+
+    linked_list.prepend(1)
+    linked_list.append(2)
+    linked_list.insert_after(1, 3)
+    linked_list.insert_before(2, 4)
+    linked_list.display()
+
+
+if __name__ == "__main__":
+    main()
